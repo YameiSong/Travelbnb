83c3ba7e5cffa430f8269403ecfafa4236be71b4
YameiSong <ymsongx@163.com>
2024-02-19 09:13:34 +1100

    create next app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      TravelBnb
+
+      <h2 className="text-airbnb">Travel</h2>
+    </main>
+  );
+}
