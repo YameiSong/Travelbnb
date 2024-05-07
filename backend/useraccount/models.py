5c0d1ed4698333db547eeb5de7c9cb5b23833357
YameiSong <ymsongx@163.com>
2024-05-07 10:35:56 +1000

    init user account app

---
+++ backend/useraccount/models.py
@@ -0,0 +1,7 @@
+import uuid
+
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+
+# Create your models here.
